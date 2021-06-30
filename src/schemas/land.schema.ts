b63f591901f90c8ca70c511d8066b0704f222cb3
Onitiana Rakotonirina <rktaoni@gmail.com>
2021-06-30 16:52:23 +0200

    [orakoto](Architecture): add schema

---
+++ src/schemas/land.schema.ts
@@ -0,0 +1,20 @@
+import { object, string, number } from 'yup';
+
+export const createLandSchema = object({
+    body: object({
+        address: string(),
+        commune: string()
+            .required(`Commune name is required`)
+            .min(2, `The commune name is to short - should be 2 chars minium`)
+            .max(75, `The commune name is to long - should be 75 chars maximum`)
+            .matches(/^[a-zA-Z0-9_.-]*$/, `Commune name can only contain Latin letters.`),
+        postalCode: number()
+            .required(`Postal code is required`)
+            .min(5, `The postal code required 5 character`)
+            .max(5, `The postal code required 5 character`),
+        surface: number()
+            .required(`Surface is required`)
+            .min(1, `Surface is to short - should be 1 chars minium`)
+            .max(25, `Surface is to long - should be 75 chars maximum`)
+    }),
+});

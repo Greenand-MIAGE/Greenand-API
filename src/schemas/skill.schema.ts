1b868581ac31dff84aa334e266c3194e6a0c31bd
GTAlon <52489318+GTAlon@users.noreply.github.com>
2021-06-30 17:37:40 +0200

    [tgerryl] (architecture) : new schema skill (#10)
    * [tgerryl] (architecture) : new schema skill
    
    * fix schema skill
    
    * fix

---
+++ src/schemas/skill.schema.ts
@@ -0,0 +1,12 @@
+  
+import { object, string, ref } from 'yup';
+
+export const createSkillSchema = object({
+  body: object({
+    label: string()
+    .required(`Label is required`)
+    .min(2, `Your label  is to short - should be 2 chars minimum`)
+    .max(25, `Your label is to long - should be 25 chars maximum`)
+    .matches(/^[a-zA-Z0-9_.-]*$/, `Label can only contain Latin letters.`),
+  }),
+});

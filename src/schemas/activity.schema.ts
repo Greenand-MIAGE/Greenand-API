8be45fff57aaefccbef1114f42ebfa549e85fe51
GTAlon <gerryl.talon@gmail.com>
2021-06-30 16:57:01 +0200

    [tgerryl] (architecture) : modif schema activity

--- src/schemas/activity.schema.ts
+++ src/schemas/activity.schema.ts
@@ -6,8 +6,7 @@ export const createActivitySchema = object({
     label: string()
     .required(`Label is required`)
     .min(3, `Your label  is to short - should be 3 chars minimum`)
-    .max(100, `Your label is to long - should be 100 chars maximum`)
-    .matches(/^[a-zA-Z0-9_.-]*$/, `Label can only contain Latin letters.`),
+    .max(100, `Your label is to long - should be 100 chars maximum`),
     clientMax: number(),
     description: string()
       .required(`Description is required`)

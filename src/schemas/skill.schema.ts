c1029d9ea19cd927dd4fac138d09a1461b14432d
GTAlon <gerryl.talon@gmail.com>
2021-06-30 17:35:25 +0200

    fix schema skill

--- src/schemas/skill.schema.ts
+++ src/schemas/skill.schema.ts
@@ -5,6 +5,8 @@ export const createSkillSchema = object({
   body: object({
     label: string()
     .required(`Label is required`)
+    .min(2, `Your label  is to short - should be 3 chars minimum`)
+    .max(25, `Your label is to long - should be 100 chars maximum`)
     .matches(/^[a-zA-Z0-9_.-]*$/, `Label can only contain Latin letters.`),
   }),
 });

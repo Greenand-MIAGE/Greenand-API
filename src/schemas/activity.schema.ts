0b28f1540f1fb19d86d9010ebe5847b3c9c6ab38
GTAlon <gerryl.talon@gmail.com>
2021-06-30 17:16:47 +0200

    fix

--- src/schemas/activity.schema.ts
+++ src/schemas/activity.schema.ts
@@ -7,7 +7,9 @@ export const createActivitySchema = object({
     .required(`Label is required`)
     .min(3, `Your label  is to short - should be 3 chars minimum`)
     .max(100, `Your label is to long - should be 100 chars maximum`),
-    clientMax: number(),
+    clientMax: number()
+    .moreThan(0,`Your number client is to short - should be 1 client minimum`)
+    .integer(`Your number is not a integer !`),
     description: string()
       .required(`Description is required`)
       .min(20,`Your description is to short - should be 20 chars minimum`),
